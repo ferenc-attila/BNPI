f02a5c4140b66b5b2447a033899eeba7ab94c8c3
ferenc-attila <attila.ferenc.80@gmail.com>
2022-03-05 12:40:47 +0100

    Update evizig_vedett_fajok_query.sql

--- obm_sql/evizig_vedett_fajok_query.sql
+++ obm_sql/evizig_vedett_fajok_query.sql
@@ -1,10 +1,10 @@
 /*BNPI teljes területe, 2010-01-01 után észlelt védett és fokozottan védett, szaporodóhelyen lévő fajok listázása helyrajzi számonként,
-adatot nem tartalmazó hrsz-ek is szerepeltetve*/
-SELECT evizig_polygons.fid AS id, group_concat(DISTINCT bnpi_species_data.fajnev) AS fajlista
+csak adatot tartalmazó hrsz-ek*/
+SELECT evizig_polygons.fid, group_concat(DISTINCT bnpi_species_data.fajnev) AS fajlista
 FROM bnpi_species_data, evizig_polygons
 WHERE st_intersects (bnpi_species_data.geom, evizig_polygons.geom)
 AND (bnpi_species_data.vedettseg = 'V' OR bnpi_species_data.vedettseg = 'FV')
-AND (bnpi_species_data >= '2010-01-01')
+AND (bnpi_species_data.datum >= '2010-01-01')
 AND (bnpi_species_data.aktivitas LIKE '%pár%'
 OR bnpi_species_data.aktivitas LIKE '%zaporod%'
 OR bnpi_species_data.aktivitas LIKE '%ad.%'
@@ -15,7 +15,6 @@ OR bnpi_species_data.aktivitas LIKE '%pelyhes%'
 OR bnpi_species_data.aktivitas LIKE '%erritor%'
 OR bnpi_species_data.aktivitas LIKE '%költőh%'
 OR bnpi_species_data.aktivitas LIKE '%tojás%')
-LEFT JOIN evizig_polygons.fid = id
 GROUP BY evizig_polygons.fid;
 
 /*DHTE specfikus lekérdezés, minden védett és fokozottan védett faj listázása helyrajzi számonként, csak adatot tartalmazó hrsz-ek*/ 
