43aa4d571e0eff50cca2152d700290bd464fe547
ferenc-attila <attila.ferenc.80@gmail.com>
2022-04-06 20:18:49 +0200

    error and info logger implementation finished

--- databackup/src/main/java/hu/bnpi/databackup/Main.java
+++ databackup/src/main/java/hu/bnpi/databackup/Main.java
@@ -1,15 +1,8 @@
 package hu.bnpi.databackup;
 
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.core.util.StatusPrinter;
-import org.slf4j.LoggerFactory;
-
 public class Main {
 
     public static void main(String[] args) {
-        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
-        StatusPrinter.print(context);
-
         WriteZipBackup writeZipBackup = new WriteZipBackup(args[0], args[1]);
         writeZipBackup.writeZip();
     }
